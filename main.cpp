8b8ff5a57e41d700848e1eda52af6a503ecd640b
mohsenmahroos <mohsenmahroos@msn.com>
2022-05-10 10:44:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,30 +6,28 @@
 #include <iostream>
 #include <numeric>
 
-using namespace std;
-using ilimits = numeric_limits<size_t>;
-using ivector = vector<size_t>;
-using ull = unsigned long long;
-
-const size_t M = 4;
-const FFT_engine<18,ivector> FFT;
-
-size_t array_size[M], f_min[M], f_max[M];
-ull K;
-vector<ivector> f(M,ivector(FFT.N));
-
-int main () {
+int main() {
+    using namespace std;
+    using i_limits = numeric_limits<size_t>;
+    using ivector = vector<size_t>;
+    using ull = unsigned long long;
+    const FFT_engine<18,ivector> FFT;
+    const size_t M = 4, N = FFT.N;
+    const size_t u_min = i_limits::min(), u_max = i_limits::max();
+    ivector array_size(M), f_min(M,u_max), f_max(M,u_min), g(N), h(N);
+    ull K;
+    vector<ivector> f(M,ivector(N));
     cin.tie(nullptr)->sync_with_stdio(false);
-    for (size_t min = ilimits::min(), max = ilimits::max(), i = 0; i < M; ++i)
-        cin >> array_size[i], f_min[i] = max, f_max[i] = min;
+    for (size_t i = 0; i < M; ++i)
+        cin >> array_size[i];
     cin >> K;
     for (size_t i = 0; i < M; ++i)
         for (size_t elem; array_size[i] > 0; ++f[i][elem])
             cin >> elem, --array_size[i],
             f_min[i] = min(f_min[i],elem),
             f_max[i] = max(f_max[i],elem);
-    auto g = FFT.convolution_sum(f[0],f[1]);
-    auto h = FFT.convolution_sum(f[2],f[3]);
+    FFT.convolution_sum(f[0],f[1],g);
+    FFT.convolution_sum(f[2],f[3],h);
     for (size_t y = 1; y < FFT.N; ++y)
         h[y] += h[y-1];
     const auto y_index = [&](ull m, ull x) {
@@ -50,6 +48,6 @@ int main () {
             else
                 r = m-1, ans = min(ans,m); }
         return ans; };
-    const auto product_of_sums = [&](size_t x[]) {
+    const auto product_of_sums = [&](const ivector &x) {
         return 1ull*(x[0]+x[1])*(x[2]+x[3]); };
     cout << binary_search(product_of_sums(f_min),product_of_sums(f_max)); }
